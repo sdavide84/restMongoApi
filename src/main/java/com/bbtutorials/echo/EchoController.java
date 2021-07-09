d953711e0c48a9afe9890ff00a1ac679f9ee2118
d.mazza <d.mazza@reply.it>
2021-07-09 15:31:46 +0200

    aggiunto query param alla chiamata base

--- src/main/java/com/bbtutorials/echo/EchoController.java
+++ src/main/java/com/bbtutorials/echo/EchoController.java
@@ -2,14 +2,15 @@ package com.bbtutorials.echo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EchoController {
 	
 	@GetMapping("/echo/{name}")
-    public String echo(@PathVariable String name) {
-        return "Hello " + name;
+    public String echo(@PathVariable String name, @RequestParam String p1) {
+        return "Hello " + name + " - " + p1;
     }
 
 }
